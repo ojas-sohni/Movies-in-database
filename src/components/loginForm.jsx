626dbece60d4019430dd90762d12b21687186243
ojas-sohni <ojas.sohni@gmail.com>
2022-02-09 01:29:21 +0530

    Input sync with state

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
   handleSubmit = (e) => {
     e.preventDefault();
   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-froup">
-            <lebel htmlFor="username">Username</lebel>
-            <input id="username" type="text" className="form-control" />
+            <label htmlFor="username">Username</label>
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              name="username"
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-froup">
-            <lebel htmlFor="password">Password</lebel>
-            <input id="password" type="text" className="form-control" />
+            <label htmlFor="password">Password</label>
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
