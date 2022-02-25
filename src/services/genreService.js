d82361ad78cb9d2ac2cf4b529d88f90a3c65b900
ojas-sohni <ojas.sohni@gmail.com>
2022-02-25 15:55:01 +0530

    MovieForm updated

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,7 +1,8 @@
 import http from "./httpService";
+import config from "../config.json";
 
 export function getGenres() {
-  return http.get("http://localhost:3900/api/genres");
+  return http.get(config.apiUrl + "/genres");
 }
 
 //We should not store the link here, Instead it should be stored in config file.
