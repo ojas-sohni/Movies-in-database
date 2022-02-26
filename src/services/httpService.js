a67c9c77e68886dfe94f0cc2bf01bd198a62f194
ojas-sohni <ojas.sohni@gmail.com>
2022-02-27 00:58:18 +0530

    fixed bidirectional dependencies b\w http service and auth service

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,10 +1,5 @@
 import axios from "axios";
-import auth from "./authService";
 import { toast } from "react-toastify";
-
-axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
-//By this we are telling axios, whenever you are making any http request, include this header in the request.
-//otherwise we will be getting a 401 error fo unauthorization while editing movie form.
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -20,9 +15,17 @@ axios.interceptors.response.use(null, (error) => {
 
 //http interceptors are the default configurations that are added automatically to every request or response that a user receives. It is useful to check response status code for every response that is being received.
 //If there is any error, control will pass from here to catch block.
+
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
+//By this we are telling axios, whenever you are making any http request, include this header in the request.
+//otherwise we will be getting a 401 error fo unauthorization while editing movie form.
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
+  setJwt,
 };
