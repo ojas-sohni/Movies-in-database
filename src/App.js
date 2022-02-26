853a62620948b49a329bda88543a08b365235d94
ojas-sohni <ojas.sohni@gmail.com>
2022-02-26 22:31:12 +0530

    Logout implemented (delete token from local storage)

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   state = {};
@@ -32,6 +34,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm}></Route>
             <Route path="/login" component={LoginForm}></Route>
+            <Route path="/logout" component={Logout}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
